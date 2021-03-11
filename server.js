ccb68f2cc9f05dc61ad61fea34c2a3410c4bffa7
Dogukan Arslan <dogukan2arslan@hotmail.com>
2021-03-11 22:00:11 +0300

    Add CORS

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
-const app = express();
+var allowCrossDomain = function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Methods", "*");
+  next();
+};
 
 app.use(express.static(__dirname + "/views"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(allowCrossDomain);
 
 const dbURI = "mongodb://127.0.0.1:27017/messages";
 
