3aa9849e2507aaf4f192d2e3e87b336846fd3209
Dogukan Arslan <dogukan2arslan@hotmail.com>
2021-03-13 09:01:32 +0300

    Add env flie

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const http = require("http").createServer(app);
 const mongoose = require("mongoose");
 const io = require("socket.io")(http);
+require("dotenv").config();
 
 var allowCrossDomain = function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -16,17 +17,6 @@ app.use(express.static(__dirname));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(allowCrossDomain);
-
-const dbURI = "mongodb://127.0.0.1:27017/messages";
-
-mongoose
-  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then((result) => {
-    const server = http.listen(process.env.PORT || 3000, () => {
-      console.log("server is runnning on port ", server.address().port);
-    });
-  })
-  .catch((error) => console.log(error));
 
 const Message = mongoose.model("Message", {
   name: String,
@@ -49,3 +39,12 @@ app.post("/messages", (req, res) => {
     res.sendStatus(200);
   });
 });
+
+mongoose
+  .connect(process.env.DB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => {
+    const server = http.listen(process.env.PORT || 3000, () => {
+      console.log("server is runnning on port ", server.address().port);
+    });
+  })
+  .catch((error) => console.log(error));
